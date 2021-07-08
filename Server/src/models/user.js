93f1f6ae03f265e5d33e261b6d004e5ae3b93619
Mrin <mrinmoym247@gmail.com>
2021-07-08 15:57:59 +0530

    Created RestAPI for user signup

---
+++ Server/src/models/user.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const {Schema} = mongoose;
+
+//Create user schema
+const userSchema= new Schema({
+    firstName:{
+        type: String,
+        require: true,
+        trim: true,
+        min: 3,
+        max: 20,
+    },
+    lastName:{
+        type: String,
+        require: true,
+        trim: true,
+        min: 3,
+        max: 20,
+    },
+    userName:{
+        type: String,
+        require: true,
+        trim: true,
+        unique: true,
+        index: true,
+        lowercase: true
+    },
+    email:{
+        type: String,
+        require: true,
+        trim: true,
+        unique: true,
+        lowercase: true
+    },
+    hash_password:{
+        type: String,
+        required: true
+    },
+    role:{
+        type: String,
+        enum: ['user','admin'],
+        default: 'user'
+    },
+    contactNumber: {type: String},
+    profilePicture: {type: String},
+},{timestamps:true});
+
+
+// userSchema methods
+userSchema.virtual('password').set(function(password){
+    this.hash_password= bcrypt.hashSync(password,10);
+});
+
+userSchema.methods= {
+    authenticate: function(password){
+        return bcrypt.compareSync(password,this.hash_password);
+    }
+};
+
+// exports userSchema as User model
+module.exports = mongoose.model('User', userSchema);
---
+++ Server/src/server.js
@@ -0,0 +1,45 @@
+//  Install dependecies
+const express = require('express');
+const env = require('dotenv');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+//  Configure environment variables
+env.config()
+
+
+//  Import internal dependencies
+const userRoute = require('./routes/user');
+
+
+// MongoDb Connection
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.pwkc0.mongodb.net/${process.env.MONGO_DB_DBNAME}?retryWrites=true&w=majority`, 
+    {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }
+).then(()=>{
+    console.log("Database is connected");
+});
+
+//  Use dependencies
+const app=express();
+
+//  Middlewares
+app.use(bodyParser());
+app.use("/api",userRoute);
+
+
+//  API ==>GET requests
+
+
+//  API <== POST requests
+
+
+
+//  satrt server
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`);
+})
