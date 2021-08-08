60e254cc76e8e21a12d3242a1f83e18989f0740c
Mrin <mrinmoym247@gmail.com>
2021-08-08 13:04:21 +0530

    Completetd update/add category actions and reducers

--- client-admin/src/store/index.js
+++ client-admin/src/store/index.js
@@ -1,12 +1,16 @@
 // ! Import libraries
 import { applyMiddleware, createStore } from "redux";
-import thunk from 'redux-thunk';
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 // ! Import Reducer module
-import rootReducer from '../reducers';
+import rootReducer from "../reducers";
 
 // ! Create Redux store
-const store = createStore(rootReducer,applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 // ! Export store
 export default store;
